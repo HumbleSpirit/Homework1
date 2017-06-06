7a5cc77511ddbf4b2b117830a10eda4934557980
HumbleSpirit <borodkin@gmail.com>
2017-06-06 19:25:30 +0300

    Second

---
+++ src/homework1/HomeWork1.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+/**
+ *
+ * @author sergey
+ */
+public class HomeWork1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        System.out.println("\tResume:\n\nName: Sergey\nSurname: Borodkin\nAge: 42\nHobby: Networking\n\n");
+        System.out.println("Task 2");
+        System.out.println("   ---\n /     \\\n/\t\\\n|\t|\n|\t|\n|\t|\n\\\t/\n \\     /\n   ---\n\n");
+        System.out.println("Task 3");
+        System.out.println("\"To be \n\tor not\n\t\tto be!\"\n\t\t\t/Shakespeare/\n");
+        System.out.println("Task 4");
+        System.out.println("\tEscape sequences\n\n\\a    Bell (alert)\n\\b    Backspace\n\\n " +
+               "   New line\n\\r    Carriage return\n\\t    Horizontal tab\n\\\\    Backslash \\ \n\\\"" + 
+               "    "
+                + "Double quotation mark \"\n\\\'    Single quotation mark \'\n");
+        System.out.println("Task 5");
+      System.out.println(" ##### ######  #####    #####   ######  ##   ##      #####     ###    #####     ###    #####   #     #  #  #     # ");
+      System.out.println("#      #       #    #  #     #  #         # #        #    #  #     #  #    #  #     #  #    #  #    #   #  ##    # ");
+      System.out.println("#      #       #    # #         #          #         #    # #       # #    # #       # #     # #   #    #  # #   # ");
+      System.out.println(" ####  ######  #####  #         ######     #         ###### #       # #####  #       # #     # ####     #  #  #  # ");
+      System.out.println("     # #       #   #  #    #### #          #         #    # #       # #   #  #       # #     # #   #    #  #   # # ");
+      System.out.println("     # #       #    #  #     #  #          #         #    #  #     #  #    #  #     #  #    #  #    #   #  #    ## ");
+      System.out.println("#####  ####### #    #   #####   #######    #         #####     ###    #    #    ###    #####   #     #  #  #     #\n ");
+      System.out.println("Task 6");
+        System.out.println("\" \'\\t\', \'\\n\', \'\\\\', \'\\'\', \'\\\"\',\n");        
+        System.out.println("Task 7");
+        System.out.println ("        ********************\n" +
+                            "       *|                * *\n" +
+                            "      * |               *  *\n" +
+                            "     *  |              *   *\n" +
+                            "    *   |             *    *\n" +
+                            "   *    |            *     *\n" +
+                            "  *     |           *      *\n" +
+                            " *******************       *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |           *      *\n" +
+                            "*       |___________*______*\n" +
+                            "*      /            *      *\n" +
+                            "*     /             *     *\n" +
+                            "*    /              *    *\n" +
+                            "*   /               *   *\n" +
+                            "*  /                *  *\n" +
+                            "* /                 **\n" +
+                            "*********************");
+    }
+    
+}
